3dcc8412e5bcb0397ebcffce0a45bed6d2c39291
Luzza-bmp <lujathapa99@gmail.com>
2025-11-22 21:19:18 +0545

    Adding changes

---
+++ flappy.js
@@ -0,0 +1,12 @@
+//board
+let board;
+let boardwidth = 360;
+let boardheight = 640;
+let context;
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.width = boardwidth;
+    board.height = boardheight;
+    context = board.getContext("2d"); // used for drawing on the board
+    
