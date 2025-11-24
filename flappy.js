8e97a64e8dd7970765363bf2b3f0626f25bbf1e1
Luzza-bmp <lujathapa99@gmail.com>
2025-11-24 20:55:35 +0545

    js modification

--- flappy.js
+++ flappy.js
@@ -19,6 +19,16 @@ let mimansh={
     height: mimanshheight
 }
 
+//pipes
+let pipearray = [];
+let pipewidth = 64;
+let pipeheight = 512;
+let pipex = boardwidth;
+let pipey = 0;
+
+let toppipeimg;
+let bottompipeimg;
+
 window.onload = function() {
     board = document.getElementById("board");
     board.width = boardwidth;
@@ -30,10 +40,30 @@ window.onload = function() {
    // context.fillRect(mimansh.x, mimansh.y, mimansh.width, mimansh.height);
    
 
-    //load mimansh image
+    //load images
     mimanshimg = new Image();
     mimanshimg.src = "./images/mimansh.png";
     mimanshimg.onload = function() {
         context.drawImage(mimanshimg, mimansh.x, mimansh.y, mimansh.width, mimansh.height);
    }
+   toppipeimg = new Image();
+  toppipeimg.src = "./images/pillar2.png.png";
+   
+  bottompipeimg = new Image();
+  bottompipeimg.src = "./images/pillar.png.png";
+   
+    requestAnimationFrame(update);
+    this.setInterval(placepipe, 1500);//every 1.5 seconds
 }
+
+ function update() {
+    requestAnimationFrame(update);
+    context.clearRect(0, 0, boardwidth, boardheight);
+    
+    //mimansh bird
+    context.drawImage(mimanshimg, mimansh.x, mimansh.y, mimansh.width, mimansh.height);
+
+}
+
+
+   
