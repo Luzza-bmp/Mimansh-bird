8caea698101286787a4033115f0b45dfb05bcc12
Luzza-bmp <lujathapa99@gmail.com>
2025-11-24 21:50:23 +0545

    Added another pic, modified the js and added the velocity

--- flappy.js
+++ flappy.js
@@ -6,7 +6,7 @@ let context;
 
 //mimansh
 let mimanshwidth = 80;
-let mimanshheight = 75;
+let mimanshheight = 60;
 let mimanshx = boardwidth / 8;
 let mimanshy = boardheight / 2;
 let mimanshimg;
@@ -21,13 +21,20 @@ let mimansh={
 
 //pipes
 let pipearray = [];
-let pipewidth = 64;
-let pipeheight = 512;
+let pipewidth = 120;
+let pipeheight = 230;
 let pipex = boardwidth;
 let pipey = 0;
 
 let toppipeimg;
 let bottompipeimg;
+
+
+//physics
+let velocityx = -2; //pipe moving left
+
+
+
 
 window.onload = function() {
     board = document.getElementById("board");
@@ -44,16 +51,20 @@ window.onload = function() {
     mimanshimg = new Image();
     mimanshimg.src = "./images/mimansh.png";
     mimanshimg.onload = function() {
+        
         context.drawImage(mimanshimg, mimansh.x, mimansh.y, mimansh.width, mimansh.height);
    }
-   toppipeimg = new Image();
-  toppipeimg.src = "./images/pillar2.png.png";
+    
+   
+    toppipeimg = new Image();
+    toppipeimg.src = "./images/toppillar.png";
    
   bottompipeimg = new Image();
-  bottompipeimg.src = "./images/pillar.png.png";
+  bottompipeimg.src = "./images/bottompillar.png";
    
     requestAnimationFrame(update);
-    this.setInterval(placepipe, 1500);//every 1.5 seconds
+    setInterval(placepipe, 1500);//every 1.5 seconds
+
 }
 
  function update() {
@@ -63,7 +74,25 @@ window.onload = function() {
     //mimansh bird
     context.drawImage(mimanshimg, mimansh.x, mimansh.y, mimansh.width, mimansh.height);
 
+    //pipes
+    for (let i = 0; i < pipearray.length; i++) {
+        let pipe = pipearray[i];
+        pipe.x += velocityx;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+ }
+
+function placepipe() {
+
+    let toppipe = {
+        img : toppipeimg,
+        x : pipex,
+        y : pipey,
+        width : pipewidth,
+        height : pipeheight,
+        passed : false //to see if the flappy bird has passed the pipe
+
+    }
+    pipearray.push(toppipe);
+
 }
-
-
-   
